4c2a0375019476cec11189e7699d79d483f57021
Эльнас Аюпов <e.ayupov@bars-open.ru>
2017-07-13 12:26:12 +0300

    Настройка проекта

---
+++ Experiences/NoteWpfApp/NoteWpfApp/Controllers/StartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteWpfApp.Controllers
+{
+    class StartController
+    {
+        private MainWindow mainWindow { get; set; }
+        public StartController()
+        {
+            mainWindow = new MainWindow();
+        }
+    }
+}
